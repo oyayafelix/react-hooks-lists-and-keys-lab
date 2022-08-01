32e1a75c79f5df12554b1473a64ea6caf4112413
oyayafelix <oyayafelix@gmail.com>
2022-08-01 13:42:30 +0300

    Completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -9,7 +9,7 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
        <div className="technologies">{technologiesList}</div>
-    </div>
+    </div>    
   );
 }
 
